2d0c640bfe68d0c246f08cc8d02103f3ff34e0bf
youks3 <ceng547969cui23@163.com>
2025-01-29 17:22:27 +0800

    test

--- src/eda_qt/main.cpp
+++ src/eda_qt/main.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "mainwindow.h"
 
 #include <QApplication>
@@ -9,3 +10,16 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
+=======
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+>>>>>>> bd1e03bf8f08e9d7e5b165ba9b21a76e52608e50
--- src/eda_qt/new_file.h
+++ src/eda_qt/new_file.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #ifndef NEW_FILE_H
 #define NEW_FILE_H
 
@@ -23,3 +24,30 @@ private:
 };
 
 #endif // NEW_FILE_H
+=======
+#ifndef NEW_FILE_H
+#define NEW_FILE_H
+
+#include <QDialog>
+
+namespace Ui {
+class new_file;
+}
+
+class new_file : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit new_file(QWidget *parent = nullptr);
+    ~new_file();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::new_file *ui;
+};
+
+#endif // NEW_FILE_H
+>>>>>>> bd1e03bf8f08e9d7e5b165ba9b21a76e52608e50
