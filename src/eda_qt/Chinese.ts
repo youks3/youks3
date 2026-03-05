3328cef2fb77a08eba8ac1edf4a1009a75500695
youks3 <ceng547969cui23@163.com>
2026-03-05 22:59:59 +0800

    增加多语言

---
+++ src/eda_qt/Chinese.ts
@@ -0,0 +1,338 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished">主窗口</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="46"/>
+        <location filename="mainwindow.ui" line="433"/>
+        <source>New Module</source>
+        <translation type="unfinished">新建模块</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="53"/>
+        <location filename="mainwindow.ui" line="60"/>
+        <location filename="mainwindow.ui" line="67"/>
+        <location filename="mainwindow.ui" line="74"/>
+        <location filename="mainwindow.ui" line="81"/>
+        <location filename="mainwindow.ui" line="88"/>
+        <location filename="mainwindow.ui" line="95"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="152"/>
+        <source>Popular</source>
+        <translation type="unfinished">常用</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="165"/>
+        <source>Examples</source>
+        <translation type="unfinished">示例</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="232"/>
+        <source>Search</source>
+        <translation type="unfinished">搜索</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="245"/>
+        <source>Port Property</source>
+        <translation type="unfinished">端口属性</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="269"/>
+        <source>Module Property</source>
+        <translation type="unfinished">模块属性</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="309"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;SimSun&apos;; font-size:15pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="334"/>
+        <source>Edit</source>
+        <translation type="unfinished">编辑</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="340"/>
+        <source>File</source>
+        <translation type="unfinished">文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="344"/>
+        <source>New</source>
+        <translation type="unfinished">新建</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="352"/>
+        <source>Export…</source>
+        <translation type="unfinished">导出...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="369"/>
+        <source>Code</source>
+        <translation type="unfinished">代码</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="376"/>
+        <source>Help</source>
+        <translation type="unfinished">帮助</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="382"/>
+        <source>Tools</source>
+        <translation type="unfinished">工具</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="390"/>
+        <source>Language</source>
+        <translation type="unfinished">语言</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="408"/>
+        <source>Open</source>
+        <translation type="unfinished">打开</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="413"/>
+        <source>Save</source>
+        <translation type="unfinished">保存</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="418"/>
+        <source>Save As ...</source>
+        <translation type="unfinished">另存为...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="423"/>
+        <source>Print</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="428"/>
+        <source>Exit</source>
+        <translation type="unfinished">退出</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="438"/>
+        <source>New Constrain</source>
+        <translation type="unfinished">新建约束文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="443"/>
+        <source>New Testbench</source>
+        <translation type="unfinished">新建测试文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="448"/>
+        <source>Code Generate</source>
+        <translation type="unfinished">生成代码</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="453"/>
+        <source>Code View</source>
+        <translation type="unfinished">代码查看器</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="458"/>
+        <source>Log</source>
+        <translation type="unfinished">日志</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="463"/>
+        <source>About</source>
+        <translation type="unfinished">关于</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="468"/>
+        <source>Clear Log</source>
+        <translation type="unfinished">清空日志</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="473"/>
+        <source>Project</source>
+        <translation type="unfinished">工程</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="478"/>
+        <source>Add to popular</source>
+        <translation type="unfinished">添加到常用区</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="483"/>
+        <source>Chinese</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="488"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="386"/>
+        <source>Preferences</source>
+        <translation type="unfinished">首选项</translation>
+    </message>
+</context>
+<context>
+    <name>about_Dialog</name>
+    <message>
+        <location filename="about_dialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="about_dialog.ui" line="31"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Website:&lt;a href=&quot;http://59.110.233.183&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;59.110.233.183&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;网站:&lt;a href=&quot;http://59.110.233.183&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;59.110.233.183&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>code_editor_dialog</name>
+    <message>
+        <location filename="code_editor_dialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>new_constrain</name>
+    <message>
+        <location filename="new_constrain.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="new_constrain.ui" line="29"/>
+        <source>Ok</source>
+        <translation type="unfinished">确定</translation>
+    </message>
+    <message>
+        <location filename="new_constrain.ui" line="49"/>
+        <source>Cancel</source>
+        <translation type="unfinished">取消</translation>
+    </message>
+    <message>
+        <location filename="new_constrain.ui" line="70"/>
+        <source>New Constrain</source>
+        <translation type="unfinished">新建约束文件</translation>
+    </message>
+    <message>
+        <location filename="new_constrain.ui" line="91"/>
+        <source>Module Name:</source>
+        <translation type="unfinished">名称：</translation>
+    </message>
+    <message>
+        <location filename="new_constrain.ui" line="105"/>
+        <source>Input Ports:</source>
+        <translation type="unfinished">Input端口数：</translation>
+    </message>
+    <message>
+        <location filename="new_constrain.ui" line="119"/>
+        <source>Output Ports:</source>
+        <translation type="unfinished">Output端口数：</translation>
+    </message>
+    <message>
+        <location filename="new_constrain.ui" line="133"/>
+        <source>InOut Ports:</source>
+        <translation type="unfinished">InOut端口数：</translation>
+    </message>
+</context>
+<context>
+    <name>new_module</name>
+    <message>
+        <location filename="new_module.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="new_module.ui" line="34"/>
+        <source>Module Name:</source>
+        <translation type="unfinished">名称：</translation>
+    </message>
+    <message>
+        <location filename="new_module.ui" line="48"/>
+        <source>Input Ports:</source>
+        <translation type="unfinished">Inout端口数：</translation>
+    </message>
+    <message>
+        <location filename="new_module.ui" line="62"/>
+        <source>Output Ports:</source>
+        <translation type="unfinished">Output端口数：</translation>
+    </message>
+    <message>
+        <location filename="new_module.ui" line="76"/>
+        <source>InOut Ports:</source>
+        <translation type="unfinished">InOut端口数：</translation>
+    </message>
+    <message>
+        <location filename="new_module.ui" line="100"/>
+        <source>Ok</source>
+        <translation type="unfinished">确定</translation>
+    </message>
+    <message>
+        <location filename="new_module.ui" line="120"/>
+        <source>Cancel</source>
+        <translation type="unfinished">取消</translation>
+    </message>
+    <message>
+        <location filename="new_module.ui" line="141"/>
+        <source>New Module</source>
+        <translation type="unfinished">新建模块文件</translation>
+    </message>
+</context>
+<context>
+    <name>new_testbench</name>
+    <message>
+        <location filename="new_testbench.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="new_testbench.ui" line="34"/>
+        <source>Module Name:</source>
+        <translation type="unfinished">测名称：</translation>
+    </message>
+    <message>
+        <location filename="new_testbench.ui" line="48"/>
+        <source>Input Ports:</source>
+        <translation type="unfinished">Input端口数：</translation>
+    </message>
+    <message>
+        <location filename="new_testbench.ui" line="62"/>
+        <source>Output Ports:</source>
+        <translation type="unfinished">Output端口数：</translation>
+    </message>
+    <message>
+        <location filename="new_testbench.ui" line="76"/>
+        <source>InOut Ports:</source>
+        <translation type="unfinished">InOut端口数：</translation>
+    </message>
+    <message>
+        <location filename="new_testbench.ui" line="102"/>
+        <source>New Testbench</source>
+        <translation type="unfinished">新建测试文件</translation>
+    </message>
+    <message>
+        <location filename="new_testbench.ui" line="118"/>
+        <source>Ok</source>
+        <translation type="unfinished">确定</translation>
+    </message>
+    <message>
+        <location filename="new_testbench.ui" line="138"/>
+        <source>Cancel</source>
+        <translation type="unfinished">取消</translation>
+    </message>
+</context>
+</TS>
